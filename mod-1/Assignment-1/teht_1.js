3ee3ef1b97151a1525a6c191b5e066e2f5a6bdb4
jaysson18 <jahaka999@gmail.com>
2023-11-15 11:46:08 +0200

    edits

---
+++ mod-1/Assignment-1/teht_1.js
@@ -0,0 +1,1 @@
+console.log("I'm Printing to console");
---
+++ mod-1/Assignment-2/teht_2.js
@@ -0,0 +1,4 @@
+'use strict'
+
+const nimi = prompt("What is your name?");
+document.querySelector('#vastaus21').innerHTML = `Hello, ${nimi}`;
